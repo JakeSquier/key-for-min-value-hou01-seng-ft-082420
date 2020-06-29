0e3869ebe432c6399b7fd14db9140a7141a19578
Jacob Squier <jacob.squier@gmail.com>
2020-06-29 03:42:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowestId = nil
+  lowestNum = name_hash
+  name_hash.each{ |id, val|
+    if val < lowestNum
+      lowestNum = val
+      lowestId = id
+    end
+  }
+  return lowestId
 end
